c1b6441a74175a5f48c0ff49da7e8461eb951289
IamLAM <ingeniero@luismonge.com.mx>
2019-06-11 00:36:18 -0700

    its ok increment function implemented

--- code.js
+++ code.js
@@ -1,7 +1,29 @@
+var breakInc=5;
+
+
 $(document).ready(function(){
 
 console.log("file linked");
 
+$("#break-increment").on("click",breakIncrement);
+$("#break-decrement").on("click",breakDecrement);
+
 
 
 });
+
+
+
+function breakIncrement(){
+
+    breakInc++;
+    $("#break-length").html(breakInc);
+
+}
+
+function breakIncrement(){
+
+    breakInc++;
+    $("#break-length").html(breakInc);
+
+}
