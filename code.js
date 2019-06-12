46cf6b336f284ddc97613754c114565a525890a5
IamLAM <ingeniero@luismonge.com.mx>
2019-06-12 02:46:38 -0700

    if you press pause and play, the counter will continue in the same value

--- code.js
+++ code.js
@@ -60,63 +60,96 @@ function sessionDecrement(){
 }
 
 function sessionStart(){
-    console.log("Inside");
     time_left= $("#time-left").html();
     console.log("Time-left-in:"+time_left);
     state=$("#state").html();
    
-    if(state=="Starting"||state=="None"){
-     
-    time_left= $("#time-left").html();
-    session = 60 * parseInt(time_left, 10);
-   
-    console.log("time:"+session);
-    $("#time").html(session);
+    switch(state){
+      case "None":
+          time_left= $("#time-left").html();
+          console.log(parseInt(time_left, 10));
+          session = 60 * parseInt(time_left, 10);
+          console.log("("+state+")");  
+            console.log("time:"+session);
+          $("#time").html(session);
+      
+          interval=setInterval(function(){
+            console.log("interval function");
+          
+          session--;
+          console.log(session);
+          if(session<0){
+            label=$("#timer-label").html();
+            if(label!="Break"){
+              $("#timer-label").html("Break");
+              session=parseInt($("#break-length").html(),10)*60;
+              $("#beep").trigger("play");
+            }else{
+              $("#timer-label").html("Session");
+              session=parseInt($("#session-length").html(),10)*60;
+              $("#beep").trigger("play");
+            }
+          }
+          min=parseInt(session/60,10);
+          sec=parseInt(session%60,10);
+          min=("0"+min).slice(-2);
+          sec=("0"+sec).slice(-2);
+          $("#time-left").html(min+":"+sec);
+      
+          },1000);
+          changeState();
+          console.log("S");
+      break;
+      case "Starting":
+          interval=setInterval(function(){
+            console.log("interval function");
+          
+          session--;
+          console.log(session);
+          if(session<0){
+            label=$("#timer-label").html();
+            if(label!="Break"){
+              $("#timer-label").html("Break");
+              session=parseInt($("#break-length").html(),10)*60;
+              $("#beep").trigger("play");
+            }else{
+              $("#timer-label").html("Session");
+              session=parseInt($("#session-length").html(),10)*60;
+              $("#beep").trigger("play");
+            }
+          }
+          min=parseInt(session/60,10);
+          sec=parseInt(session%60,10);
+          min=("0"+min).slice(-2);
+          sec=("0"+sec).slice(-2);
+          $("#time-left").html(min+":"+sec);
+      
+          },1000);
+          changeState();
+          console.log("S");
+      break;
 
-    interval=setInterval(function(){
-      console.log("interval function");
-    
-    session--;
-    console.log(session);
-    if(session<0){
-      label=$("#timer-label").html();
-      if(label!="Break"){
-        $("#timer-label").html("Break");
-        session=parseInt($("#break-length").html(),10)*60;
-        $("#beep").trigger("play");
-      }else{
-        $("#timer-label").html("Session");
-        session=parseInt($("#session-length").html(),10)*60;
-        $("#beep").trigger("play");
-      }
-    }
-    min=parseInt(session/60,10);
-    sec=parseInt(session%60,10);
-    min=("0"+min).slice(-2);
-    sec=("0"+sec).slice(-2);
-    $("#time-left").html(min+":"+sec);
-
-    },1000);
-    changeState();
-  }else{
-    console.log("state changed");
+      default:
+          console.log("state changed");
     changeState();
     clearInterval(interval);
     time_left= $("#time-left").html();
     console.log("Time-left-out:"+time_left);
     console.log(session);
     $("#time").html(session);
-  }
+      break;
+    }
+ 
 }
 
 function changeState(){
   if ($("#state").html() == "Starting"||$("#state").html() == "None") {
     
     $("#state").html("Stop");
-    console.log("Stop...");
+    console.log("(Stop)");
   } else {
      $("#state").html("Starting");
-     console.log("Starting...");
+     console.log("(Starting)");
   }
 }
 function resetState(){
@@ -127,6 +160,7 @@ function resetState(){
   $("#break-length").html(breakk);
   $("#time-left").html(session+":00");
   $("#state").html("None");
+  console.log("(None)");
   clearInterval(interval);
   $("#beep").trigger("pause");
   $("#beep").prop("currentTime",0);
